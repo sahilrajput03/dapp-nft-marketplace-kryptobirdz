7e06312630b23f482f51a22480e8f5b73c53d69a
Sahil Rajput <sahilrajput03@gmail.com>
2022-08-16 21:01:44 +0530

    up

--- contracts/learn-contracts/inheritance-and-import/5_4.sol
+++ contracts/learn-contracts/inheritance-and-import/5_4.sol
@@ -5,30 +5,34 @@ pragma solidity ^0.8.16;
 // 1. https://github.com/NomicFoundation/hardhat/issues/557
 // 2. https://github.com/NomicFoundation/hardhat/issues/649
 
-// ? WATCH FILE 5_4.sol instead ~Sahil
-
-/*
 
 // import "./5_2_1.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol"
+import '@openzeppelin/contracts/access/Ownable.sol';
+
 
 import "hardhat/console.sol";
 
 // 
 // THIS IS A DEMO FOR USING INTERITANCE FOR BENEFIT OF REUSING CODE
-// => THIS FILE IS A DEMO FOR THE USAGE OF `import` with github file link.
+// => THIS FILE IS A DEMO FOR THE USAGE OF `import` with the openzepplin library's contract via npm.
 // 
 
 
-// Note: We are inheriting `owned` contract in `Contract5_2`
-contract Contract5_3 is owned{
+// Note: We are inheriting `Ownable` contract in `Contract5_2`
+contract Contract5_4 is Ownable {
     uint public balance; // initial value is 0
 
     // Adding money to contract address from thin air IMO ~Sahil
     function getMoney()public payable{
-        if(msg.sender != owner){
-            revert("Not owner");
-        }
+
+        
+        //!LEARN: Below throws error like: Operator != not compatible with types address and function () view returns (address).
+        //!LEARN: This happens becoz we are inheriting `Ownable` contract from @openzepplin's contracts library ~Sahil
+        //!LEARN: (This happens *idk exactly* ~Sahil)
+        // if(msg.sender != owner){
+        //     revert("Not owner");
+        // }
         balance += msg.value;
     }
     // Transfer money from contract to given account ~Sahil
@@ -47,5 +51,3 @@ contract Contract5_3 is owned{
     //     selfdestruct(address);
     // }
 }
-
-*/
